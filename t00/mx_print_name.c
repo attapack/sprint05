d7c81fe2c1e4816583ca27275955ecb8aab12cfc
attapack <75312811+attapack@users.noreply.github.com>
2021-09-17 11:59:40 +0300

    Update mx_print_name.c

--- t00/mx_print_name.c
+++ t00/mx_print_name.c
@@ -3,8 +3,8 @@ void mx_printint(int n);
 void mx_printstr(const char *s);
 
 int main(int argc, char *argv[] )  {
-   mx_printstr(argv[0]);
-   mx_printchar('\n');
-   mx_printint(argc);
-   mx_printchar('\n');
+    mx_printstr(argv[0]);
+    mx_printchar('\n');
+    mx_printint(argc);
+    mx_printchar('\n');
 }
